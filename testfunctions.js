7ef728cd8eb70035a36fe617a3facc69efc9eeac
Talwaa <daxwsword@gmail.com>
2026-05-23 15:08:15 -0700

    color change + javascript in seperate file

---
+++ testfunctions.js
@@ -0,0 +1,16 @@
+function colorChange() {
+                        document.body.style.backgroundColor = "black"
+                    }
+function uncolorChange() {
+    document.body.style.backgroundColor = "white"
+}
+function jstester() {
+                            const truePassword = "Sword33"
+                            const input = document.getElementById("password").value
+                            if (input == truePassword) {
+                                window.location.href="linked.html"
+                            } else {
+                                alert("Incorrect Password")
+                                document.getElementById("password").value = ""
+                            }
+                        }
