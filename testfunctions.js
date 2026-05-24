d5e624d9a963bf3ba9313b233f64437494112f8f
Talwaa <daxwsword@gmail.com>
2026-05-23 17:37:27 -0700

    more div experimentation

--- testfunctions.js
+++ testfunctions.js
@@ -5,12 +5,12 @@ function uncolorChange() {
     document.body.style.backgroundColor = "white"
 }
 function jstester() {
-                            const truePassword = "Sword33"
-                            const input = document.getElementById("password").value
-                            if (input == truePassword) {
-                                window.location.href="linked.html"
-                            } else {
-                                alert("Incorrect Password")
-                                document.getElementById("password").value = ""
-                            }
-                        }
+    const truePassword = "Sword33"
+    const input = document.getElementById("password").value
+    if (input == truePassword) {
+        window.location.href="linked.html"
+    } else {
+            alert("Incorrect Password")
+            document.getElementById("password").value = ""
+    }
+}
